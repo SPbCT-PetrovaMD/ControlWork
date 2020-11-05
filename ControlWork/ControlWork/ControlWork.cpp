1ef5ff9da3aa36001acad143b33cebcae56fdcc5
SPbCT-PetrovaMD <74009743+SPbCT-PetrovaMD@users.noreply.github.com>
2020-11-05 20:21:45 +0300

    First commit

---
+++ ControlWork/ControlWork/ControlWork.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable : 4996)
+using namespace std;
+
+int main()
+{
+	FILE* fp1;
+	char ch1;
+	if ((fp1 = fopen("Eng.txt", "r")) == NULL) {
+		printf("Cannot open file.\n");
+		exit(1);
+	}
+	while ((ch1 = fgetc(fp1)) != EOF) {
+		printf("%c", ch1);
+		printf(" ");
+	}
+	fclose(fp1);
+	
+	printf("\n-----------------------------------------\n");
+
+	FILE* fp2;
+	char ch2;
+	if ((fp2 = fopen("Ru.txt", "r")) == NULL) {
+		printf("Cannot open file.\n");
+		exit(1);
+	}
+	while ((ch2 = fgetc(fp2)) != EOF) {
+		printf("%c", ch2);
+		printf(" ");
+	}
+	fclose(fp2);
+
+	printf("\n");
+
+	return 0;
+}
